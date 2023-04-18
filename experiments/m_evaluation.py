da00b221685c3b33579bbe4ad27eb14f01140711
Adri <carrasquilla.adria@gmail.com>
2023-04-18 20:27:17 +0200

    CLEANUP: moved some files not being used rn to experiments

---
+++ experiments/m_evaluation.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2021, InterDigital R&D France. All rights reserved.
+#
+# This source code is made available under the license found in the
+# LICENSE.txt in the root directory of this source tree.
+
+import argparse
+import os
+import numpy as np
+import torch
+import yaml
+
+from PIL import Image
+from torchvision import utils
+
+from datasets import *
+from trainer import *
+from utils.functions import *
+
+torch.backends.cudnn.enabled = True
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+torch.autograd.set_detect_anomaly(True)
+Image.MAX_IMAGE_PIXELS = None
+DEVICE = torch.device('cuda')
+
+'No_Beard'
+'Eyeglasses'
+'Big_Nose'
+'Heavy_Makeup'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='multi_sum_loss2', help='Path to the config file.')
+parser.add_argument('--attr', type=str, default='No_Beard,Chubby', help='attribute for manipulation.')
+parser.add_argument('--latent_path', type=str, default='./data/celebahq_dlatents_psp.npy', help='dataset path')
+parser.add_argument('--label_file', type=str, default='./data/celebahq_anno.npy', help='label file path')
+parser.add_argument('--stylegan_model_path', type=str, default='./pixel2style2pixel/pretrained_models/psp_ffhq_encode.pt', help='stylegan model path')
+parser.add_argument('--classifier_model_path', type=str, default='./models/latent_classifier_epoch_20.pth', help='pretrained attribute classifier')
+parser.add_argument('--log_path', type=str, default='./logs/', help='log file path')
+opts = parser.parse_args()
+
+# Celeba attribute list
+attr_dict = {'5_o_Clock_Shadow': 0, 'Arched_Eyebrows': 1, 'Attractive': 2, 'Bags_Under_Eyes': 3, \
+            'Bald': 4, 'Bangs': 5, 'Big_Lips': 6, 'Big_Nose': 7, 'Black_Hair': 8, 'Blond_Hair': 9, \
+            'Blurry': 10, 'Brown_Hair': 11, 'Bushy_Eyebrows': 12, 'Chubby': 13, 'Double_Chin': 14, \
+            'Eyeglasses': 15, 'Goatee': 16, 'Gray_Hair': 17, 'Heavy_Makeup': 18, 'High_Cheekbones': 19, \
+            'Male': 20, 'Mouth_Slightly_Open': 21, 'Mustache': 22, 'Narrow_Eyes': 23, 'No_Beard': 24, \
+            'Oval_Face': 25, 'Pale_Skin': 26, 'Pointy_Nose': 27, 'Receding_Hairline': 28, 'Rosy_Cheeks': 29, \
+            'Sideburns': 30, 'Smiling': 31, 'Straight_Hair': 32, 'Wavy_Hair': 33, 'Wearing_Earrings': 34, \
+            'Wearing_Hat': 35, 'Wearing_Lipstick': 36, 'Wearing_Necklace': 37, 'Wearing_Necktie': 38, 'Young': 39}
+
+########################################################################################################################
+# Generate manipulated samples for evaluation
+# For the evaluation data, we project the first 1K images of FFHQ into the the latent space W+ of StyleGAN. 
+# For each input image, we edit each attribute with 10 different scaling factors and generate the corresponding images.
+########################################################################################################################
+
+# Load input latent codes
+testdata_dir = '/home/adri/Projects/latent-transformer/data/test/'
+n_steps = 11
+scale = 2.0
+
+with torch.no_grad():
+    
+    save_dir = './outputs/evaluation/'
+    os.makedirs(save_dir, exist_ok=True)
+
+    log_dir = os.path.join(opts.log_path, opts.config) + '/'
+    config = yaml.safe_load(open('./configs/' + opts.config + '.yaml', 'r'))
+
+    attr1, attr2 = opts.attr.split(',')
+    attrs = [attr1, attr2]
+    attr_num = [attr_dict[attr1], attr_dict[attr2]]
+
+    # Initialize trainer
+    trainer = Trainer(config, attr_num, attrs, opts.label_file)
+    trainer.initialize(opts.stylegan_model_path, opts.classifier_model_path)   
+    trainer.to(DEVICE)
+
+    trainer.load_model(log_dir)
+    
+    for k in range(1000):
+
+        w_0 = np.load(testdata_dir + 'latent_code_%05d.npy' % k)
+        w_0 = torch.tensor(w_0).to(DEVICE)
+
+        predict_lbl_0 = trainer.Latent_Classifier(w_0.view(w_0.size(0), -1))
+        lbl_0 = torch.sigmoid(predict_lbl_0)
+        attr_pb_0 = lbl_0[torch.arange(lbl_0.shape[0]), attr_num]
+
+        coeff = torch.zeros(attr_pb_0.size())
+        for i in range(len(attr_pb_0)):
+            coeff[i] = -1 if attr_pb_0[i] > 0.5 else 1
+
+        range_alpha = torch.linspace(0, scale*coeff[0].item(), n_steps)
+        for i,alpha in enumerate(range_alpha):
+
+            coeff = torch.tensor([[alpha, alpha]]).to(DEVICE)
+            
+            w_1 = trainer.T_net(w_0.view(w_0.size(0), -1), coeff) # , alpha.unsqueeze(0).to(device))
+            w_1 = w_1.view(w_0.size())
+            w_1 = torch.cat((w_1[:,:11,:], w_0[:,11:,:]), 1)
+            x_1, _ = trainer.StyleGAN([w_1], input_is_latent=True, randomize_noise=False)
+            utils.save_image(clip_img(x_1), save_dir + attr1 + '_%d'%k  + '_alpha_'+ str(i) + '.jpg')
+        break
---
+++ experiments/overall_test.py
@@ -0,0 +1,159 @@
+# Copyright (c) 2021, InterDigital R&D France. All rights reserved.
+#
+# This source code is made available under the license found in the
+# LICENSE.txt in the root directory of this source tree.
+
+import argparse
+import os
+import numpy as np
+import torch
+import yaml
+import matplotlib.pyplot as plt
+from rich.progress import track
+import random
+random.seed(1)
+
+from PIL import Image
+from torchvision import utils
+
+from trainer import Trainer as MultiTrainer
+from original_trainer import Trainer as SingleTrainer
+from utils.functions import *
+from constants import ATTR_TO_NUM
+from evaluation import compute_sequential_loss
+
+torch.backends.cudnn.enabled = True
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+torch.autograd.set_detect_anomaly(True)
+Image.MAX_IMAGE_PIXELS = None
+
+from constants import DEVICE
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='less_attrs', help='Path to the config file.')
+parser.add_argument('--attr', type=str, default='Bald,No_Beard,Smiling', help='attribute for manipulation.')
+parser.add_argument('--latent_path', type=str, default='./data/celebahq_dlatents_psp.npy', help='dataset path')
+parser.add_argument('--label_file', type=str, default='./data/celebahq_anno.npy', help='label file path')
+parser.add_argument('--stylegan_model_path', type=str, default='./pixel2style2pixel/pretrained_models/psp_ffhq_encode.pt', help='stylegan model path')
+parser.add_argument('--classifier_model_path', type=str, default='./models/latent_classifier_epoch_20.pth', help='pretrained attribute classifier')
+parser.add_argument('--log_path', type=str, default='./logs/', help='log file path')
+parser.add_argument('--out_path', type=str, default='./outputs/', help='output path')
+opts = parser.parse_args()
+
+
+log_dir = os.path.join(opts.log_path, opts.config) + '/'
+config = yaml.safe_load(open('./configs/' + opts.config + '.yaml', 'r'))
+save_dir = opts.out_path + f'test/{opts.config}/'
+os.makedirs(save_dir, exist_ok=True)
+log_dir_single = os.path.join(opts.log_path, "original_train") + '/'
+
+local_attr_dict = {attr:i for i, attr in enumerate(config["attr"].split(","))}
+
+
+attributes = {'Bald': 1, 'Smiling': 1, 'Male': -1, 'No_Beard': -1,
+              'Arched_Eyebrows': 1, 'Eyeglasses': 1, 'Wearing_Lipstick':-1}
+
+# attributes = {attr: random.choice([-1,1]) for attr in local_attr_dict.keys()}
+# print(attributes)
+
+scales = [1, 1.5, 2, 3]
+all_losses = [{s: {"loss": [], "loss_pb": [], "loss_reg": [], "loss_recon": []} for s in scales + ["Single"]} for _ in range(2)]
+print(all_losses)
+
+attrs = config['attr'].split(',')
+attr_num = [ATTR_TO_NUM[a] for a in attrs]
+
+with torch.no_grad():
+    # We start with MultiAttribute trainer
+    for s in track(scales, "Evaluating different coeff scaling values"):
+        # Initialize trainer
+        trainer = MultiTrainer(config, attr_num, attrs, opts.label_file)
+        trainer.initialize(opts.stylegan_model_path, opts.classifier_model_path)   
+        trainer.load_model_multi(log_dir, "20_attrs")
+        trainer.to(DEVICE)
+        
+        for i in [0,1]:
+            w_0 = np.load(f'./data/teaser/latent_code_0000{i+1}.npy')
+            w_0 = torch.tensor(w_0).to(DEVICE)
+            x_0, _ = trainer.StyleGAN([w_0], input_is_latent=True, randomize_noise=False) 
+            img_l = [x_0]
+
+            coeffs = torch.zeros((1,len(attrs))).to(DEVICE)
+            for _, (attr, coeff) in enumerate(attributes.items()):
+                attr_idx = local_attr_dict[attr]
+                coeffs[0][attr_idx] = coeff
+
+                w_1 = trainer.T_net(w_0.view(w_0.size(0), -1), coeffs, scaling=s)
+                w_1 = w_1.view(w_0.size())
+
+                loss, loss_pb, loss_reg, loss_recon = compute_sequential_loss(w_0, w_1, attr_num, coeffs.squeeze(0), trainer)
+
+                all_losses[i][s]["loss"].append(loss.item())
+                all_losses[i][s]["loss_pb"].append(loss_pb.item())
+                all_losses[i][s]["loss_reg"].append(loss_reg.item())
+                all_losses[i][s]["loss_recon"].append(loss_recon.item())
+
+                w_1 = torch.cat((w_1[:,:11,:], w_0[:,11:,:]), 1)
+                x_1, _ = trainer.StyleGAN([w_1], input_is_latent=True, randomize_noise=False)
+                img_l.append(x_1.data)
+
+            out = torch.cat(img_l, dim=3)
+            utils.save_image(clip_img(out), save_dir + f"test_multi_{i}_{s}.png")
+
+    # Now its time for Single Attribute way
+    trainer = SingleTrainer(config, None, None, opts.label_file)
+    trainer.initialize(opts.stylegan_model_path, opts.classifier_model_path)   
+    trainer.to(DEVICE)
+    
+    for i in [0,1]:
+        w_0 = np.load(f'./data/teaser/latent_code_0000{i+1}.npy')
+        w_0 = torch.tensor(w_0).to(DEVICE)
+        w_prev = w_0
+        w_1 = w_0
+        x_0, _ = trainer.StyleGAN([w_0], input_is_latent=True, randomize_noise=False) 
+        img_l = [x_0]
+
+        coeffs = torch.zeros((1,len(attrs))).to(DEVICE)
+        for _, (attr, coeff) in track(enumerate(attributes.items()), "Evaluating Single Model"):
+            attr_idx = local_attr_dict[attr]
+            coeffs[0][attr_idx] = coeff
+
+            trainer.attr_num = ATTR_TO_NUM[attr]
+            trainer.load_model(log_dir_single)
+            trainer.to(DEVICE)
+
+            w_1 = trainer.T_net(w_prev.view(w_0.size(0), -1), torch.tensor(coeff).unsqueeze(0).to(DEVICE))
+            w_1 = w_1.view(w_0.size())
+            w_prev = w_1
+
+            loss, loss_pb, loss_reg, loss_recon = compute_sequential_loss(w_0, w_1, attr_num, coeffs.squeeze(0), trainer)
+
+            all_losses[i]["Single"]["loss"].append(loss.item())
+            all_losses[i]["Single"]["loss_pb"].append(loss_pb.item())
+            all_losses[i]["Single"]["loss_reg"].append(loss_reg.item())
+            all_losses[i]["Single"]["loss_recon"].append(loss_recon.item())
+
+            w_1 = torch.cat((w_1[:,:11,:], w_0[:,11:,:]), 1)
+            x_1, _ = trainer.StyleGAN([w_1], input_is_latent=True, randomize_noise=False)
+            img_l.append(x_1.data)
+
+        out = torch.cat(img_l, dim=3)
+        utils.save_image(clip_img(out), save_dir + f"test_multi_{i}_Single.png")
+
+for i in range(2):
+    for l in ["loss", "loss_pb", "loss_reg", "loss_recon"]:
+        plt.figure(figsize=(12,6))
+        for s in all_losses[i].keys():
+            if s == "Single":
+                label = "SingleAttr"
+            else:
+                label = f"MultiAttr: {s}"
+            plt.plot(range(1,len(attributes)+1), all_losses[i][s][l], label=label)
+            plt.title(l, fontsize=16)
+            plt.xticks(range(1,len(attributes)+1), labels=list(attributes.keys()))
+            plt.legend(fontsize=12)
+            plt.xlabel("Accumulated attributes", fontsize=12)
+            plt.ylabel("Loss", fontsize=12)
+        plt.savefig(f"{save_dir}overall_{i}_{l}.png", bbox_inches="tight")
+        plt.clf()
