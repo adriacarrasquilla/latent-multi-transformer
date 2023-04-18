e5968f17d3512b60c02279806c7acb767cf264f6
Adri <carrasquilla.adria@gmail.com>
2023-04-18 19:51:56 +0200

    CLEANUP: trainer.py

--- trainer.py
+++ trainer.py
@@ -42,8 +42,6 @@ class Trainer(nn.Module):
         # To control inference only
         self.scaling = scaling
 
-        # self.attr_num = attr_num[0]
-        # self.attr = attr[0]
         mapping_lrmul = self.config['mapping_lrmul']
         mapping_layers = self.config['mapping_layers']
         mapping_fmaps = self.config['mapping_fmaps']
@@ -115,10 +113,6 @@ class Trainer(nn.Module):
             lbls = np.load(self.label_file)
             self.corr_ma = np.corrcoef(lbls.transpose())
             self.corr_ma[np.isnan(self.corr_ma)] = 0
-            # np.save("corr.npy", self.corr_ma)
-        # corr_vec = np.abs(self.corr_ma[attr_num:attr_num+1]) # Original, below is the experimental one
-        # Enable this again for exp
-        # attr_num = attr_num.cpu()
 
         corr_vec = np.abs(self.corr_ma[attr_num, :])
         corr_vec[corr_vec>=threshold] = 1
@@ -144,61 +138,11 @@ class Trainer(nn.Module):
         sign_1 = F.relu(0.5-x).sign()
 
         coeffs = sign_0*(-x) + sign_1*(1-x)
-        scenario = 0 # random.randint(0,2)
 
-        if scenario == 0:
-            attrs_idx = torch.randint(0, coeffs.shape[0], (1,), device=DEVICE)
-            coeffs[torch.arange(coeffs.shape[0], device=DEVICE) != attrs_idx] = 0
-        elif scenario == 1:
-            n = random.randint(2, coeffs.shape[0] - 1)
-            attrs_idx = random.sample(range(coeffs.shape[0] -1), n)
-            coeffs[torch.tensor(attrs_idx)] = coeffs[torch.tensor(attrs_idx)]  # set the chosen elements to their original values
-            coeffs[torch.tensor(list(set(range(coeffs.shape[0])) - set(attrs_idx)))] = 0
-        else:
-            # Do nothing, use all the attributes
-            pass
+        attrs_idx = torch.randint(0, coeffs.shape[0], (1,), device=DEVICE)
+        coeffs[torch.arange(coeffs.shape[0], device=DEVICE) != attrs_idx] = 0
 
         return coeffs
-
-    def compute_loss(self, w, mask_input, n_iter):
-        self.w_0 = w
-        predict_lbl_0 = self.Latent_Classifier(self.w_0.view(w.size(0), -1))
-        lbl_0 = torch.sigmoid(predict_lbl_0)
-        attr_pb_0 = lbl_0[:, self.attr_num]
-
-        # Get scaling factor
-        coeff = self.get_coeff(attr_pb_0)
-        target_pb = torch.clamp(attr_pb_0 + coeff, 0, 1).round()
-
-        if 'alpha' in self.config and not self.config['alpha']:
-            coeff = 2 * target_pb.type_as(attr_pb_0) - 1 
-        # Apply latent transformation
-        self.w_1 = self.T_net(self.w_0.view(w.size(0), -1), coeff, scaling=self.scaling)
-        self.w_1 = self.w_1.view(w.size())
-        predict_lbl_1 = self.Latent_Classifier(self.w_1.view(w.size(0), -1))
-
-        # Pb loss
-        T_coeff = target_pb.size(0)/(target_pb.sum(0) + 1e-8)
-        F_coeff = target_pb.size(0)/(target_pb.size(0) - target_pb.sum(0) + 1e-8)
-
-        mask_pb = T_coeff.float() * target_pb + F_coeff.float() * (1-target_pb)
-        self.loss_pb = self.BCEloss(predict_lbl_1[:, self.attr_num], target_pb, reduction='none')*mask_pb
-        self.loss_pb = self.loss_pb.mean()
-
-        # Latent code recon
-        self.loss_recon = self.MSEloss(self.w_1, self.w_0)
-
-        # Reg loss
-        threshold_val = 1 if 'corr_threshold' not in self.config else self.config['corr_threshold']
-        mask = torch.tensor(self.get_correlation(self.attr_num, threshold=threshold_val)).type_as(predict_lbl_0)
-        mask = mask.repeat(predict_lbl_0.size(0), 1)
-        self.loss_reg = self.MSEloss(predict_lbl_1*mask, predict_lbl_0*mask)
-        
-        # Total loss
-        w_recon, w_pb, w_reg = self.config['w']['recon'], self.config['w']['pb'], self.config['w']['reg']
-        self.loss =  w_pb * self.loss_pb + w_recon*self.loss_recon + w_reg * self.loss_reg
-
-        return self.loss
 
     def compute_eval_loss(self, w, coeff):
         self.w_0 = w
@@ -215,11 +159,8 @@ class Trainer(nn.Module):
         predict_lbl_1 = self.Latent_Classifier(self.w_1.view(w.size(0), -1))
 
         # Pb loss
-        T_coeff = target_pb.size(0)/(target_pb.sum(0) + 1e-8)
-        F_coeff = target_pb.size(0)/(target_pb.size(0) - target_pb.sum(0) + 1e-8)
-        mask_pb = T_coeff.float() * target_pb + F_coeff.float() * (1-target_pb)
         self.loss_pb = self.BCEloss(predict_lbl_1[torch.arange(predict_lbl_1.shape[0]), self.attr_nums],
-                                    target_pb, reduction='none')*mask_pb
+                                    target_pb, reduction='none')
         self.loss_pb = self.loss_pb.mean()
 
         # Latent code recon
@@ -251,33 +192,13 @@ class Trainer(nn.Module):
         target_pb = torch.clamp(attr_pb_0 + coeff, 0, 1).round()
         target_pb = target_pb[coeff_idx]
 
-        mask_pb = torch.ones_like(coeff)  # TODO: train with this, I used zeros before
-        mask_pb[coeff_idx] = mask_pb.shape[0]
-
-        if 'alpha' in self.config and not self.config['alpha']:
-            coeff = 2 * target_pb.type_as(attr_pb_0) - 1 
-            
-        # coeff_scaled = 10 * coeff.type_as(attr_pb_0)
-
         # Apply latent transformation
         self.w_1 = self.T_net(self.w_0.view(w.size(0), -1), coeff.unsqueeze(0), scaling=self.scaling)
         self.w_1 = self.w_1.view(w.size())
         predict_lbl_1 = self.Latent_Classifier(self.w_1.view(w.size(0), -1))
 
-        # Pb loss (experimental)
-        # self.loss_pb = self.BCEloss(predict_lbl_1[torch.arange(predict_lbl_1.shape[0]), self.attr_nums],
-        #                             target_pb, reduction='mean')
-        # self.loss_pb = self.MultiLabelLoss(predict_lbl_1[torch.arange(predict_lbl_1.shape[0]), self.attr_nums],
-        #                             target_pb, reduction='mean')
-
-        # Original loss_pb down here
-        # Pb loss
-        T_coeff = target_pb.size(0)/(target_pb.sum(0) + 1e-8)
-        F_coeff = target_pb.size(0)/(target_pb.size(0) - target_pb.sum(0) + 1e-8)
-        mask_pb_tmp = T_coeff.float() * target_pb + F_coeff.float() * (1-target_pb)
-
         pred_pb = predict_lbl_1[torch.arange(predict_lbl_1.shape[0]), self.attr_nums]
-        self.loss_pb = self.BCEloss(pred_pb[coeff_idx], target_pb, reduction='none')# *mask_pb
+        self.loss_pb = self.BCEloss(pred_pb[coeff_idx], target_pb, reduction='none')
         self.loss_pb = self.loss_pb.mean()
 
         # Latent code recon
@@ -286,7 +207,6 @@ class Trainer(nn.Module):
         # Reg loss
         threshold_val = 1 if 'corr_threshold' not in self.config else self.config['corr_threshold']
         mask = torch.tensor(self.get_correlation_multi(self.attr_nums, threshold=threshold_val, coeffs=coeff)).type_as(predict_lbl_0)
-        # mask = mask.repeat(predict_lbl_0.size(0), 1)  # We dont need to repeat the correlation mask in multi
         self.loss_reg = self.MSEloss(predict_lbl_1*mask, predict_lbl_0*mask)
         
         # Total loss
@@ -299,11 +219,9 @@ class Trainer(nn.Module):
         # Original image
         predict_lbl_0 = self.Latent_Classifier(w.view(w.size(0), -1))
         lbl_0 = torch.sigmoid(predict_lbl_0)
-        # attr_pb_0 = lbl_0[:, self.attr_num] # TODO: find a global way to do this, or do separate functions
-        # self.attr_num = torch.argmax(lbl_0, axis=1)
-        # self.local_attr = NUM_TO_ATTR[self.attr_num.item()]
-        self.local_attr = "multi"
-        # attr_pb_1 = lbl_0[torch.arange(lbl_0.shape[0]), self.attr_num]
+
+        self.local_attr = "multi"  # this is for saving purposes only
+
         attr_pb_0 = lbl_0[torch.arange(lbl_0.shape[0]), self.attr_nums]
 
         coeff = self.get_coeff(attr_pb_0)
@@ -331,44 +249,6 @@ class Trainer(nn.Module):
         logger.add_image('image_'+str(self.attrs)+'/iter'+str(n_iter+1)+f'_input_{self.local_attr}', clip_img(downscale(self.x_0, 2))[0], n_iter + 1)
         logger.add_image('image_'+str(self.attrs)+'/iter'+str(n_iter+1)+f'_modif_{self.local_attr}', clip_img(downscale(self.x_1, 2))[0], n_iter + 1)
 
-    def get_image_verbose(self, w):
-        # Original image
-        predict_lbl_0 = self.Latent_Classifier(w.view(w.size(0), -1))
-        lbl_0 = torch.sigmoid(predict_lbl_0)
-        # attr_pb_0 = lbl_0[:, self.attr_num] # TODO: find a global way to do this, or do separate functions
-        self.attr_num = torch.argmax(lbl_0, axis=1)
-        self.local_attr = NUM_TO_ATTR[self.attr_num.item()]
-        # attr_pb_1 = lbl_0[torch.arange(lbl_0.shape[0]), self.attr_num]
-        attr_pb_0 = lbl_0[torch.arange(lbl_0.shape[0]), self.attr_nums]
-
-        coeff = self.get_coeff(attr_pb_0)
-        target_pb = torch.clamp(attr_pb_0 + coeff, 0, 1).round()
-
-        if 'alpha' in self.config and not self.config['alpha']:
-            coeff = 2 * target_pb.type_as(attr_pb_0) - 1 
-
-        w_1 = self.T_net(w.view(w.size(0), -1), coeff.unsqueeze(0), scaling=self.scaling)
-        w_1 = w_1.view(w.size())
-        self.x_0, _ = self.StyleGAN([w], input_is_latent=True, randomize_noise=False)
-        self.x_1, _ = self.StyleGAN([w_1], input_is_latent=True, randomize_noise=False)
-
-        predict_lbl_1 = self.Latent_Classifier(w_1.view(w.size(0), -1))
-
-        predict = predict_lbl_1[torch.arange(predict_lbl_1.shape[0]), self.attr_nums]
-
-        return attr_pb_0, target_pb, predict
-
-    def log_image_verbose(self, logger, w, n_iter):
-        with torch.no_grad():
-            org, target, predict = self.get_image_verbose(w)
-        suffix = ""
-        for o, t, p in zip(org, target, predict):
-            suffix += f",org_{o.item():.2f},target_{t.item():.2f},pred_{p.item():.2f}"
-        # logger.add_image('image_'+self.attr+'/iter'+str(n_iter+1)+f'_input_{self.local_attr}', clip_img(downscale(self.x_0, 2))[0], n_iter + 1)
-        # logger.add_image('image_'+self.attr+'/iter'+str(n_iter+1)+f'_modif_{self.local_attr}', clip_img(downscale(self.x_1, 2))[0], n_iter + 1)
-        logger.add_image('image_'+str(self.attrs)+'/iter'+str(n_iter+1)+'input', clip_img(downscale(self.x_0, 2))[0], n_iter + 1)
-        logger.add_image('image_'+str(self.attrs)+'/iter'+str(n_iter+1)+'modif'+suffix, clip_img(downscale(self.x_1, 2))[0], n_iter + 1)
-        
     def log_loss(self, logger, n_iter):
         logger.add_scalar('loss_'+str(self.attrs)+'/class', self.loss_pb.item(), n_iter + 1)
         logger.add_scalar('loss_'+str(self.attrs)+'/latent_recon', self.loss_recon.item(), n_iter + 1)
@@ -426,4 +306,3 @@ class Trainer(nn.Module):
         self.optimizer.zero_grad()
         self.compute_loss_multi(w, mask, n_iter).backward()
         self.optimizer.step()
-        
