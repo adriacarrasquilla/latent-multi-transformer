34a196226da0896cfeafc5672d4a9594399df1cb
Adri <carrasquilla.adria@gmail.com>
2023-04-26 15:35:07 +0200

    Finished subjective study methods

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,12 +1,18 @@
 import random
+from rich.progress import track
 from torchvision import os
 import yaml
 
 import numpy as np
 import torch
-from constants import ATTR_TO_NUM
+from constants import ATTR_TO_NUM, NUM_TO_ATTR, ATTR_TO_SPANISH
+from evaluation import apply_transformation, get_trainer
+from torchvision import utils
 
 from nets import LCNet
+from trainer import Trainer as MultiTrainer
+from original_trainer import Trainer as SingleTrainer
+from utils.functions import clip_img
 
 random.seed(1)
 testdata_dir = "./data/ffhq/"
@@ -78,19 +84,90 @@ def attributes_order_dataset(n_samples=n_samples, all_attrs=all_attrs):
 
 
 def subjective_study(n_imgs=20, all_attrs=all_attrs):
-    for k in random.sample(range(n_samples), n_imgs):
-        print(k)
-        local_attrs = random.sample(range(len(all_attrs)), random.randint(1,len(all_attrs)))
-        global_attrs = [all_attrs[a] for a in local_attrs]
-        # TODO: finish this so that:
-        # * We choose a random number of attributes to modify (maybe use weights so that we get more samples with less attrs)
-        # * We transform the randomly selected sample with the Single and Multi transformers
-        # * Save the images into a directory
-        # * Generate a text file with the attributes modified and wether it was a positive or negative transformation
 
+    random.seed(666)
+    out = "./data/subjective/"
+    os.makedirs(out + "img/", exist_ok=True)
+    os.makedirs(out + "txt/", exist_ok=True)
+
+    all_coeffs = np.load(testdata_dir + "labels/all.npy")
+    narrow_done = False
+    for i, k in track(enumerate(random.sample(range(n_samples), n_imgs)), "Generating Samples"):
+
+        attr_idx = list(range(len(all_attrs)))
+
+        if narrow_done:
+            # Narrow eyes are too present in the results. Engineering this to remove it (they both do equally good)
+            attr_idx.remove(16)
+
+        local_attrs = random.sample(attr_idx, random.randint(1, len(all_attrs)))
+
+        if 16 in local_attrs:
+            narrow_done = True
+
+        coeff = torch.zeros(all_coeffs[k].shape).to(DEVICE)
+        coeff[local_attrs] = torch.tensor(all_coeffs[k][local_attrs], dtype=torch.float).to(DEVICE)
+
+        w_0 = np.load(testdata_dir + "latent_code_%05d.npy" % k)
+        w_0 = torch.tensor(w_0).to(DEVICE)
+
+        # multi + original
+        trainer = get_trainer(multi=True)
+
+        x_0, _ = trainer.StyleGAN([w_0], input_is_latent=True, randomize_noise=False)
+        utils.save_image(clip_img(x_0), out + f"img/{i+1}_original.jpg")
+
+        w_1 = apply_transformation(trainer, w_0, coeff, multi=True)
+        w_1 = torch.cat((w_1[:,:11,:], w_0[:,11:,:]), 1)
+        x_1, _ = trainer.StyleGAN([w_1], input_is_latent=True, randomize_noise=False)
+        utils.save_image(clip_img(x_1), out + f"img/{i+1}_multi.jpg")
+
+        # single
+        trainer = get_trainer(multi=False)
+        w_1 = apply_transformation(trainer, w_0, coeff, multi=False)
+        w_1 = torch.cat((w_1[:,:11,:], w_0[:,11:,:]), 1)
+        x_1, _ = trainer.StyleGAN([w_1], input_is_latent=True, randomize_noise=False)
+        utils.save_image(clip_img(x_1), out + f"img/{i+1}_single.jpg")
+
+        with open(out + f"txt/{i+1}_attrs.txt", "w") as f:
+            for attr in local_attrs:
+                f.write(f"{NUM_TO_ATTR[all_attrs[attr]]}: {coeff[attr]}\n")
+
+def subjective_form_assets(n_imgs=20, AB=False):
+    out = "./data/subjective/form/"
+    os.makedirs(out, exist_ok=True)
+
+    if AB:
+        with open(out + "transformer_order.csv", "w") as f:
+            f.write("question,multi,single\n")
+            for i in range(n_imgs):
+                multi, single = random.sample(["A", "B"], 2)
+                f.write(f"{i+1},{multi},{single}\n")
+
+    for i in range(1,21):
+        print(f"Question {i}")
+        operations = {"add": [], "remove": []}
+        operations_spanish = {"Añadimos": [], "Eliminamos": []}
+        with open("./data/subjective/best/txt/" + f"{i}_attrs.txt", "r") as f:
+            for line in f.readlines():
+                attr, coeff = line[:-1].split(":")
+                op = 'remove' if "-" in coeff else 'add'
+                op_s = 'Eliminamos' if "-" in coeff else 'Añadimos'
+                operations[op].append(attr)
+                operations_spanish[op_s].append(ATTR_TO_SPANISH[attr])
+
+        for key in operations_spanish.keys():
+            print(f"{key} los siguientes atributos: {', '.join(operations_spanish[key])}")
+
+        print()
+        for key in operations.keys():
+            print(f"We {key} the following attributes: {', '.join(operations[key])}")
+
+        print("-------------------------------------------------------------------")
 
 
 # individual_dataset()
 # overall_dataset()
 # attributes_order_dataset()
-subjective_study()
+# subjective_study()
+subjective_form_assets()
