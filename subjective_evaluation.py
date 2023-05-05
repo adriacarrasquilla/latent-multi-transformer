a2a7d78f9bc5aa88647f591702ef840d29860439
Adri <carrasquilla.adria@gmail.com>
2023-05-05 10:31:47 +0200

    Plotting results of subjective study

--- subjective_evaluation.py
+++ subjective_evaluation.py
@@ -1,11 +1,13 @@
+import os
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-matplotlib.use("tkagg")
 
 subj_dir = "./data/subjective/form/"
+out_dir = "./outputs/evaluation/subjective/"
+os.makedirs(out_dir, exist_ok=True)
 
 answers = pd.read_csv(subj_dir + "answers.csv")
 
@@ -20,16 +22,14 @@ true_labels = true_labels.pivot_table(index='question', columns='value', values=
 true_labels.columns = ['A', 'B']
 
 
+# Get scores per model (number of choices per image)
 scores = {"single": np.zeros(20), "multi": np.zeros(20), "Ambas / Both": np.zeros(20)}
-scores_org = {"A": np.zeros(20), "B": np.zeros(20), "Ambas / Both": np.zeros(20)}
 for col in answers:
     for i, row_value in answers[col].items():
         if row_value not in ["A", "B"]:
             scores["Ambas / Both"][col-1] += 1
-            scores_org["Ambas / Both"][col-1] += 1
         else:
             scores[true_labels[row_value][col]][col-1] += 1
-            scores_org[row_value][col-1] += 1
 
 scores = pd.DataFrame(scores)
 
@@ -41,7 +41,7 @@ question_pct = scores.apply(lambda x: x / x.sum() * 100, axis=1)
 fig = plt.figure(figsize=(10,10))
 plt.pie(overall_pct, labels=scores.columns, autopct='%1.1f%%', textprops={'size': 14})
 plt.title('Overall Percentages by Column', fontsize=15)
-plt.savefig(subj_dir + "overall_pie.png")
+plt.savefig(out_dir + "overall_pie.png")
 
 # Bar plot for question percentages
 ax = question_pct.plot(kind='bar', figsize=(20,10))
@@ -49,12 +49,44 @@ ax.set_title('Percentages of choice per question', fontsize=15)
 ax.set_xlabel('Question Number', fontsize=12)
 ax.set_ylabel('Choice percentage (%)', fontsize=12)
 ax.legend()
-plt.savefig(subj_dir + "question_pct.png")
+plt.savefig(out_dir + "question_pct.png")
 
 
-max_counts = question_pct.idxmax(axis=1).value_counts()
-print(max_counts)
+# Print how many times each option has been the most voted one
+overall_winner = question_pct.idxmax(axis=1).value_counts().to_frame().transpose()
 
+# Print how many times each option, excluding "Both", has been the most voted one
 tmp = question_pct.drop(columns=["Ambas / Both"])
-max_counts = tmp.idxmax(axis=1).value_counts()
-print(max_counts)
+model_winner = tmp.idxmax(axis=1).value_counts().to_frame().transpose()
+
+
+# Count how many times each person has voted each option
+scores = [{"single":0, "multi":0, "Ambas / Both":0} for _ in range(len(answers.index))]
+for i, row in answers.iterrows():
+    for quest, ans in enumerate(row):
+        if ans in ["A", "B"]:
+            scores[i][true_labels.iloc[quest][ans]] += 1
+        else:
+            scores[i]["Ambas / Both"] += 1
+# for score in scores:
+#     print(score)
+
+person_winner = pd.DataFrame(scores).idxmax(axis=1).value_counts().to_frame().transpose()
+
+df = pd.DataFrame(columns=['single', 'multi', 'Ambas / Both'])
+
+for i, winner in enumerate([overall_winner, model_winner, person_winner]):
+    df.loc[i] = [winner[col].iloc[0] if col in winner.columns else 0 for col in df.columns]
+
+group_names = ['Most chosen\noption', 'Most chosen\nmodel', 'Option preference\nby person']
+
+# create the bar plot
+ax = df.plot.bar(rot=0, figsize=(10, 7))
+ax.set_ylabel('Times an option was chosen', fontsize=12)
+ax.set_xlabel('Types of comparisons', fontsize=12)
+ax.set_xticklabels(group_names)
+ax.set_title("Counts of winning scenarios (most chosen option)", fontsize=15)
+ax.legend(fontsize=10)
+
+plt.tight_layout()
+plt.savefig(out_dir + "winners.png")
